32da05335563c44a3e60e947cc7623cebaa3a094
LifeSnake <fayllissnake@gmail.com>
2025-07-21 15:52:05 +0300

    Added Test game area size of 10X20. Need to add cells itself and their logic.

--- GameOfLife.c
+++ GameOfLife.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#define width 24
+#define height 12
+int gameMatrix[height][width];
 
 int main(void)
 {
-    printf("YES");
+    for(int i = 0;i < height;i++){
+        for(int j = 0;j < width;j++){
+            gameMatrix[i][j] = ' ';
+            if(i == 0 || i == height - 1) gameMatrix[i][j] = '_';
+            if((j == 0 || j == width - 1) && i != 0) gameMatrix[i][j] = '|';
+            printf("%c",gameMatrix[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
